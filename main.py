a249351f5b7b5b61ea2c7535b807f34a2922444b
LeoLindqvist123 <leolikro01@gmail.com>
2026-02-25 10:23:32 +0100

    Comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import onnxruntime as ort
 import numpy as np
 
 app = FastAPI()
-#Ladda ONNX-modellen
+#Load ONNX-modellen
 model = ort.InferenceSession("model/simplecnn.onnx")
 
 @app.get("/")
@@ -16,20 +16,21 @@ def health():
 
 @app.post("/predict")
 def predict(data: dict):
+    # Try-except for error handling
     try:
         if "image" not in data:
             return {"error": "Missing 'image' key in data"}
         
-        # Tar emot bild från json
+        # Receiving image from json
         image = data["image"]
         
         if len(image) != 3072:
             return {"error": f"Invalid image size. Expected 3072, got {len(image)}"}
         
-        # Omvandla till rätt format [1, 3, 32, 32]
+        # Convert to the correct format [1, 3, 32, 32]
         input_array = np.array(image).reshape(1, 3, 32, 32).astype(np.float32)
         
-        # Kör modellen
+        # Run the model
         results = model.run(None, {"input": input_array})
         predictions = results[0][0]
         
