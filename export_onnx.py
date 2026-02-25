2faac2783422346aff1b365dc788a288c7a8f432
LeoLindqvist123 <leolikro01@gmail.com>
2026-02-25 08:54:03 +0100

    update

--- export_onnx.py
+++ export_onnx.py
@@ -1,1 +1,8 @@
 import torch as torch
+import sys
+sys.path.append('./src')
+from model import simpleCNN
+
+#Ladda modellen
+model = simpleCNN()
+model.load_state_dict(torch.load('./model_weights.pth'))
