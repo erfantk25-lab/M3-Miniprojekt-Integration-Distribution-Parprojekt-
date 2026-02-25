11f98a281f8a4293ada0600065f5f9b55a5b7e5e
LeoLindqvist123 <leolikro01@gmail.com>
2026-02-25 09:38:31 +0100

    Add FastAPI with predict endpoint

--- main.py
+++ main.py
@@ -15,21 +15,31 @@ def health():
     return {"status": "healthy"}
 
 @app.post("/predict")
-def predict(data: list):
-    #Tar emot bild från json
-    image = data["image"]
-    # Omvandla till rätt format [1, 3, 32, 32]
-    input_array = np.array(image).reshape(1, 3, 32, 32).astype(np.float32)
-
-    #kör modellen
-    results = model.run(None, {"input": input_array})
-    predictions = results[0][0]
-
-    class_id = int(np.argmax(predictions))
-    confidence = float(np.max(predictions))
-
-    return{
-        "class_id": class_id,
-        "confidence": confidence
-    }
-
+def predict(data: dict):  # ← Ändrat här!
+    try:
+        if "image" not in data:
+            return {"error": "Missing 'image' key in data"}
+        
+        # Tar emot bild från json
+        image = data["image"]
+        
+        if len(image) != 32*32*3:
+            return {"error": f"Invalid image size. Expected 3072, got {len(image)}"}
+        
+        # Omvandla till rätt format [1, 3, 32, 32]
+        input_array = np.array(image).reshape(1, 3, 32, 32).astype(np.float32)
+        
+        # Kör modellen
+        results = model.run(None, {"input": input_array})
+        predictions = results[0][0]
+        
+        class_id = int(np.argmax(predictions))
+        confidence = float(np.max(predictions))
+        
+        return {
+            "class_id": class_id,
+            "confidence": confidence
+        }
+    
+    except Exception as e:
+        return {"error": str(e)}
